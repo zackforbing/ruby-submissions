02e8541c72e247f27214a5886c44ec66ff333214
Jeff Casimir <jeff@casimircreative.com>
2014-11-12 10:42:01 -0700

    Layout for The Pivot 1407 submissions

---
+++ 1407/06_the_pivot/submissions.rb
@@ -0,0 +1,43 @@
+def project
+  {
+    :group1 => {  :app_name      => 'project name'
+                  :members       => ['Name1', 'Name2', 'Name3'],
+                  :repo_url      => 'https://github.com/someproject.git',
+                  :production_url=> 'none',
+                  :collaborators => ['you@example.com','you2@example.com'],
+                  :technical_evaluation => {
+                    :modeling    => 0,
+                    :interface   => 0,
+                    :performance => 0,
+                    :process     => 0,
+                    :comments    => ""
+                  }
+                },
+    :group1 => {  :app_name      => 'project name'
+                  :members       => ['Name1', 'Name2', 'Name3'],
+                  :repo_url      => 'https://github.com/someproject.git',
+                  :production_url=> 'none',
+                  :collaborators => ['you@example.com','you2@example.com'],
+                  :technical_evaluation => {
+                    :modeling    => 0,
+                    :interface   => 0,
+                    :performance => 0,
+                    :process     => 0,
+                    :comments    => ""
+                  }
+                },
+    :group1 => {  :app_name      => 'project name'
+                  :members       => ['Name1', 'Name2', 'Name3'],
+                  :repo_url      => 'https://github.com/someproject.git',
+                  :production_url=> 'none',
+                  :collaborators => ['you@example.com','you2@example.com'],
+                  :technical_evaluation => {
+                    :modeling    => 0,
+                    :interface   => 0,
+                    :performance => 0,
+                    :process     => 0,
+                    :comments    => ""
+                  }
+                }
+  }
+end
